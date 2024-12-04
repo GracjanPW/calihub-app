acec2e568ff693cd9a8554ba1da438fd11345302
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-12-04 17:23:04 +0100

    (schedule) back to current week button

--- src/app/(user)/schedule/_components/week-selector.tsx
+++ src/app/(user)/schedule/_components/week-selector.tsx
@@ -1,17 +1,9 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import {
-  addDays,
-  endOfWeek,
-  format,
-  isWithinInterval,
-  startOfWeek,
-  subDays,
-  toDate,
-} from "date-fns";
+import { ChevronLeft, ChevronRight, Filter, SkipBackIcon } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { addDays, format, isWithinInterval, subDays, toDate } from "date-fns";
 import { usePathname, useRouter } from "next/navigation";
 import { cn, getDateRange } from "@/lib/utils";
 
@@ -39,10 +31,14 @@ export const WeekSelector = ({ defaultFrom, defaultTo }: WeekSelectorProps) => {
     setDateRange({ to, from });
   };
 
+  const currentWeek = () => {
+    setDateRange(getDateRange(new Date()));
+  };
+
   const currentPeriod = useMemo(() => {
     return isWithinInterval(new Date(), {
       start: toDate(dateRange.from),
-      end: toDate(dateRange.to),
+      end: addDays(toDate(dateRange.to), 1),
     });
   }, [dateRange]);
 
@@ -65,20 +61,39 @@ export const WeekSelector = ({ defaultFrom, defaultTo }: WeekSelectorProps) => {
   }, [dateRange, pathname, router]);
 
   return (
-    <div className="flex justify-between items-center border rounded-md">
-      <Button variant={"ghost"} size={"icon"} onClick={prevWeek}>
-        <ChevronLeft />
-      </Button>
-      <p
-        className={cn(
-          "text-lg",
-          currentPeriod? "text-neutral-800 font-bold": "font-semibold"
-        )}
+    <div className="flex justify-center space-x-2">
+      <Button
+        disabled={
+          !(
+            new Date() < toDate(dateRange.from) ||
+            new Date() > toDate(dateRange.to)
+          )
+        }
+        onClick={currentWeek}
+        variant={"outline"}
+        size={"icon"}
       >
-        {currentPeriod?"This week":dateRangeString}
-      </p>
-      <Button variant={"ghost"} size={"icon"} onClick={nextWeek}>
-        <ChevronRight />
+        <SkipBackIcon />
+      </Button>
+
+      <div className="flex-1 flex justify-between items-center border rounded-md">
+        <Button variant={"ghost"} size={"icon"} onClick={prevWeek}>
+          <ChevronLeft />
+        </Button>
+        <p
+          className={cn(
+            "text-lg",
+            currentPeriod ? "text-neutral-800 font-bold" : "font-semibold"
+          )}
+        >
+          {currentPeriod ? "This week" : dateRangeString}
+        </p>
+        <Button variant={"ghost"} size={"icon"} onClick={nextWeek}>
+          <ChevronRight />
+        </Button>
+      </div>
+      <Button variant={"outline"} size={"icon"} disabled={false}>
+        <Filter />
       </Button>
     </div>
   );
