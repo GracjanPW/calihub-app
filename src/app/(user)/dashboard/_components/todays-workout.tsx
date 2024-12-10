cb75970b1bb54d3e8783b1b26a09af63d97e12c5
GracjanPW <gracjanwojciechowski2002@gmail.com>
2024-12-10 20:22:13 +0000

    fixed react/no-unescaped-entities

--- src/app/(user)/dashboard/_components/todays-workout.tsx
+++ src/app/(user)/dashboard/_components/todays-workout.tsx
@@ -13,7 +13,7 @@ export const TodaysWorkout = async () => {
       {todaysWorkout.length > 0 ? (
         <Link href={`/workout/${format(today, 'yyyy-MM-dd')}`}>
           <div className='w-full space-y-2 rounded-md border bg-neutral-100 px-4 py-3 text-center shadow-sm shadow-neutral-700'>
-            <p className='text-sm text-muted-foreground'>Today's Workout</p>
+            <p className='text-sm text-muted-foreground'>Today&apos;s Workout</p>
             <p className='text-xl font-semibold text-neutral-700'>{label}</p>
             <p className='text-xs text-muted-foreground'>
               {todaysWorkout.length} Exercises
