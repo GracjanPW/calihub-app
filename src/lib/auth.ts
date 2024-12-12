42e606202d92855eb2815ed5ddb23e16df86c238
GracjanPW <gracjanwojciechowski2002@gmail.com>
2024-12-12 21:26:31 +0000

    reduce middleware size

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,9 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import { db } from './db';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import { getUserByEmail } from './db/user';
+import { compareSync } from 'bcryptjs';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -21,4 +24,30 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   adapter: PrismaAdapter(db),
   ...authConfig,
+  providers:[
+    ...authConfig.providers,
+    Credentials({
+      credentials: {
+        email: { label: 'User' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        if (!email || !password) return null;
+        const existingUser = await getUserByEmail(email as string);
+        if (!existingUser || !existingUser.password) return null;
+        // TODO: dont allow user in if not verified
+        // if (!existingUser.emailVerified) return
+
+        const passwordsMatch = compareSync(
+          password as string,
+          existingUser.password
+        );
+
+        if (!passwordsMatch) return null;
+
+        return existingUser;
+      },
+    }),
+  ]
 });
